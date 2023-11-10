8680993a28fbf7a38804a205b26e558c51172ce4
daxchx <daxchx.v1@gmail.com>
2023-11-10 21:51:12 +0900

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,220 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+let nextId = 0;
 
-export default function Home() {
+export default function List() {
+  const [name, setName] = useState("");
+  const [count, setCount] = useState<any>("");
+  const [tableType, setTableType] = useState("テーブル");
+
+  const [watings, setWatings] = useState<any>([]);
+  const [enters, setEnters] = useState<any>([]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <>
+      <div className="p-6">
+        <h1 className="text-2xl font-bold leading-7 text-gray-900">
+          Wating Board
+        </h1>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="p-6 text-sm">
+        <input
+          className="py-1 px-2 border rounded bg-gray-50"
+          value={name}
+          placeholder="お名前"
+          onChange={(e) => setName(e.target.value)}
         />
+        <input
+          className="ml-2 py-1 px-2 border rounded bg-gray-50"
+          type="number"
+          value={count}
+          onChange={(e) => setCount(+e.target.value)}
+          placeholder="人数"
+        />
+        <select
+          className="ml-2 py-1 px-2 border rounded bg-gray-50"
+          value={tableType}
+          onChange={(e) => setTableType(e.target.value)}
+        >
+          <option value="テーブル">テーブル</option>
+          <option value="カウンター">カウンター</option>
+          <option value="どちらでも可">どちらでも可</option>
+        </select>
+        <button
+          className="ml-2 py-1 px-2 border border-green-500 rounded bg-green-500 text-white"
+          onClick={() => {
+            setWatings([
+              ...watings,
+              { id: nextId++, name: name, count: count, tableType: tableType },
+            ]);
+            setName("");
+            setCount("");
+            setTableType("テーブル");
+          }}
+        >
+          順番待ちに追加する
+        </button>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="p-6 border-t bg-gray-50">
+        <table className="w-full border bg-white text-sm">
+          <caption className="mb-4 text-base font-semibold text-left">
+            順番待ち
+          </caption>
+          <thead>
+            <tr className="border-b font-semibold text-gray-900">
+              <td className="w-1/4 py-3 px-4 border-r">名前</td>
+              <td className="w-1/4 py-3 px-4 border-r">人数</td>
+              <td className="w-1/4 py-3 px-4 border-r">テーブル</td>
+              <td className="w-1/4 py-3 px-4 border-r">アクション</td>
+            </tr>
+          </thead>
+          <tbody>
+            {watings.length == 0 && (
+              <tr>
+                <td className="w-1/4 py-3 px-4 border-r opacity-0">
+                  山田 太郎
+                </td>
+                <td className="w-1/4 py-3 px-4 border-r opacity-0">1</td>
+                <td className="w-1/4 py-3 px-4 border-r opacity-0">
+                  どちらでも可
+                </td>
+                <td className="w-1/4 py-3 pr-1 pl-4 opacity-0">
+                  <button disabled={true} className="py-1 px-2 border text-xs">
+                    取り消し
+                  </button>
+                  <button
+                    disabled={true}
+                    className="ml-2 py-1 px-2 border text-xs"
+                  >
+                    案内
+                  </button>
+                </td>
+              </tr>
+            )}
+            {watings.map(
+              (wating: {
+                id: number;
+                name: string;
+                count: number;
+                tableType: string;
+              }) => (
+                <tr className="border-b" key={wating.id}>
+                  <td className="py-3 px-4 border-r">{wating.name}</td>
+                  <td className="py-3 px-4 border-r text-gray-500">
+                    {wating.count}
+                  </td>
+                  <td className="py-3 px-4 border-r text-gray-500">
+                    {wating.tableType}
+                  </td>
+                  <td className="py-3 pr-1 pl-4">
+                    <button
+                      className="py-1 px-2 border border-red-500 rounded bg-red-50 text-xs text-red-500"
+                      onClick={() => {
+                        setWatings(
+                          watings.filter(
+                            (a: { id: number }) => a.id !== wating.id
+                          )
+                        );
+                      }}
+                    >
+                      取消
+                    </button>
+                    <button
+                      className="ml-2 py-1 px-2 border border-green-500 rounded bg-green-50 text-xs text-green-500"
+                      onClick={() => {
+                        setEnters([
+                          ...enters,
+                          {
+                            id: wating.id,
+                            name: wating.name,
+                            count: wating.count,
+                            tableType: wating.tableType,
+                          },
+                        ]);
+                        setWatings(
+                          watings.filter(
+                            (a: { id: number }) => a.id !== wating.id
+                          )
+                        );
+                      }}
+                    >
+                      案内
+                    </button>
+                  </td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
       </div>
-    </main>
-  )
+      <div className="p-6 border-t bg-gray-50">
+        <table className="w-full border bg-white text-sm">
+          <caption className="mb-4 text-base font-semibold text-left">
+            店内
+          </caption>
+          <thead>
+            <tr className="border-b font-semibold text-gray-900">
+              <td className="py-3 px-4 border-r">名前</td>
+              <td className="py-3 px-4 border-r">人数</td>
+              <td className="py-3 px-4 border-r">テーブル</td>
+              <td className="py-3 px-4 border-r">アクション</td>
+            </tr>
+          </thead>
+          <tbody>
+            {enters.length == 0 && (
+              <tr>
+                <td className="w-1/4 py-3 px-4 border-r opacity-0">
+                  山田 太郎
+                </td>
+                <td className="w-1/4 py-3 px-4 border-r opacity-0">1</td>
+                <td className="w-1/4 py-3 px-4 border-r opacity-0">
+                  どちらでも可
+                </td>
+                <td className="w-1/4 py-3 pr-1 pl-4 opacity-0">
+                  <button
+                    disabled={true}
+                    className="ml-2 py-1 px-2 border border-green-500 rounded bg-green-50 text-xs text-green-500"
+                  >
+                    退店
+                  </button>
+                </td>
+              </tr>
+            )}
+            {enters.map(
+              (enter: {
+                id: number;
+                name: string;
+                count: number;
+                tableType: string;
+              }) => (
+                <tr className="border-b" key={enter.id}>
+                  <td className="w-1/4 py-3 px-4 border-r">{enter.name}</td>
+                  <td className="w-1/4 py-3 px-4 border-r text-gray-500">
+                    {enter.count}
+                  </td>
+                  <td className="w-1/4 py-3 px-4 border-r text-gray-500">
+                    {enter.tableType}
+                  </td>
+                  <td className="w-1/4 py-3 px-4">
+                    <button
+                      className="py-1 px-2 border border-red-500 rounded bg-red-50 text-xs text-red-500"
+                      onClick={() => {
+                        setEnters(
+                          enters.filter(
+                            (a: { id: number }) => a.id !== enter.id
+                          )
+                        );
+                      }}
+                    >
+                      退店
+                    </button>
+                  </td>
+                </tr>
+              )
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
